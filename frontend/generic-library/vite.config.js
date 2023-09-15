38b9ae4a3185bce3538505b272a8b7c8eeb9d2eb
paulAlexSerban <paul.alex.serban@gmail.com>
2023-09-15 22:22:46 +0300

    changed base path

--- frontend/generic-library/vite.config.js
+++ frontend/generic-library/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/prj--reactjs-component-lib/',
+  base: 'https://paulalexserban.github.io/prj--reactjs-component-lib/',
 })

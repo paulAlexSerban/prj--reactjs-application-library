865e6a68bc610cdab9bd90959a5a923ca6aa5e7b
paulAlexSerban <paul.alex.serban@gmail.com>
2023-10-23 13:43:33 +0300

    feat: added storybook

---
+++ frontend/design-systems/storybook/.storybook/main.js
@@ -0,0 +1,30 @@
+
+
+import { join, dirname } from 'path';
+
+/**
+ * This function is used to resolve the absolute path of a package.
+ * It is needed in projects that use Yarn PnP or are set up within a monorepo.
+ */
+function getAbsolutePath(value){
+    return dirname(require.resolve(join(value, 'package.json')));
+}
+const config = {
+    stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+    addons: [
+        getAbsolutePath('@storybook/addon-a11y'),
+        getAbsolutePath('@storybook/addon-essentials'),
+        getAbsolutePath('@storybook/addon-interactions'),
+        getAbsolutePath('@storybook/addon-knobs'),
+        getAbsolutePath('@storybook/addon-links'),
+        getAbsolutePath('@storybook/addon-onboarding'),
+    ],
+    framework: {
+        name: getAbsolutePath('@storybook/react-webpack5'),
+        options: {},
+    },
+    docs: {
+        autodocs: 'tag',
+    },
+};
+export default config;

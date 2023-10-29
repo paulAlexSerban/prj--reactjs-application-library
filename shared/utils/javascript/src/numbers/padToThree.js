c34bef6ecbbb22f4b84162df75e056e2e3189a00
paulAlexSerban <paul.alex.serban@gmail.com>
2023-10-29 12:51:46 +0200

    feat(shared js functionw): added

---
+++ shared/utils/javascript/src/numbers/padToThree.js
@@ -0,0 +1,7 @@
+/**
+ * padToThree - adds 0s to the front of a number to make it 3 digits long
+ * @param {*} number
+ * @returns
+ */
+const padToThree = (number) => (number <= 999 ? `00${number}`.slice(-3) : number);
+export default padToThree;

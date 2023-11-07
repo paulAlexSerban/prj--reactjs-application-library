054e587ee273e2c7254472f3d072ed1f299c22c4
paulAlexSerban <paul.alex.serban@gmail.com>
2023-11-07 20:18:02 +0200

    feat: cleanup

--- frontend/apps/counter-simple-app/src/App.jsx
+++ frontend/apps/counter-simple-app/src/App.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
-import Heading from './components/Heading.jsx';
-import './global.css';
+import './index.css';
+import '@prj--reactjs-component-lib/generic-lsg/lib/global.css';
+import '@prj--reactjs-component-lib/generic-lsg/lib/atoms/button.css';
+import '@prj--reactjs-component-lib/generic-lsg/lib/molecules/counter.css';
+
+import { Counter } from '@prj--reactjs-component-lib/generic-lib';
+
 const App = () => {
-    return <Heading name="JavaScript" />;
+    return (
+        <div className="App">
+            <Counter color="lightgray" />
+        </div>
+    );
 };
 
 export default App;

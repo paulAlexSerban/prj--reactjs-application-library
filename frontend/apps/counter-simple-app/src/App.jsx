6af9b6a2f45b437e937787d2f96754d932c008bb
paulAlexSerban <paul.alex.serban@gmail.com>
2023-11-08 15:48:30 +0200

    feat: trigger

--- frontend/apps/counter-simple-app/src/App.jsx
+++ frontend/apps/counter-simple-app/src/App.jsx
@@ -3,7 +3,6 @@ import './index.css';
 import '@prj--reactjs-application-library/generic-lsg/lib/global.css';
 import '@prj--reactjs-application-library/generic-lsg/lib/atoms/button.css';
 import '@prj--reactjs-application-library/generic-lsg/lib/molecules/counter.css';
-
 import { Counter } from '@prj--reactjs-application-library/generic-lib';
 
 const App = () => {
